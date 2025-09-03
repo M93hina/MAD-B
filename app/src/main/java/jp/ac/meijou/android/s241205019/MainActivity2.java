0026c39060186c4a5ffc2fc14e9e82210b13c2b8
M93hina <90587244+M93hina@users.noreply.github.com>
2025-09-03 14:37:27 +0900

    MainActivityにいくつかのintent機能を追加

--- app/src/main/java/jp/ac/meijou/android/s241205019/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205019/MainActivity2.java
@@ -1,5 +1,6 @@
 package jp.ac.meijou.android.s241205019;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205019.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205019.databinding.ActivityMainBinding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        binding.buttonBento.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        binding.buttonYahoo.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(android.net.Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
+        });
+
+        binding.buttonSend.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity.class);
+            intent.putExtra("intent_text",binding.editTextSend.getText().toString());
+            startActivity(intent);
         });
     }
 }
